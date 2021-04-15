cc0ae2ecf678ca7fd10b1b3d0a70b399b05a1362
andy <admazzola@gmail.com>
2021-04-15 15:30:45 -0400

    fix footer

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -4,15 +4,15 @@
       <div class=" flex lg:flex-row h-full">
          <div class="lg:w-1/4 ">
          </div>
-        <div class="w-1/2 lg:w-1/4">
+        <div class="w-1/2 lg:w-1/4 flex flex-col">
           <h3 class=" text-lg font-bold ">Documentation</h3>
 
-    <a href="https://etherscan.io/address/0x167152a46e8616d4a6892a6afd8e52f060151c70" target="_blank" class="no-underline text-sm text-gray-200 ">Guild Contract</a>
-   <a href="https://etherscan.io/address/0x657223e3fdf539d92c40664db340097d5d6bd9f5" target="_blank" class="no-underline text-sm text-gray-200 ">Reserve Token</a>
-        
-        
+          <a href="https://etherscan.io/address/0x167152a46e8616d4a6892a6afd8e52f060151c70" target="_blank" class="no-underline text-sm text-gray-200 ">Guild Contract</a>
+           <a href="https://etherscan.io/address/0x657223e3fdf539d92c40664db340097d5d6bd9f5" target="_blank" class="no-underline text-sm text-gray-200 ">Reserve Token</a>
+              
+              
           </div>
-        <div class="w-1/2 lg:w-1/4">
+        <div class="w-1/2 lg:w-1/4 flex flex-col">
           <h3 class=" text-lg font-bold ">Community &amp; Social</h3>
            <a href="https://0xbitcoin.org" target="_blank" class="no-underline text-sm text-gray-200 ">0xBTC Website</a>
          
