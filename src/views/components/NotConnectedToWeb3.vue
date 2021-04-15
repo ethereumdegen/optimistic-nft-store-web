051149dffd896262063077c99a8b833e8753bb41
andy <admazzola@gmail.com>
2021-04-15 14:32:23 -0400

    connection div

--- src/views/components/NotConnectedToWeb3.vue
+++ src/views/components/NotConnectedToWeb3.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="section  ">
-    <div class="container-3 w-container">
+    <div class="m-2 w-container w-1/2 border-2 border-black inline-block">
        
         
-          <h3 class="heading-3 footer">No web3 connection found</h3>
+          <h3 class="  p-2 bg-gray-200 text-lg text-red-700">No web3 connection found.</h3>
 
 
            
        
-        <div class=" ">
+        <div class="  p-2 text-sm text-gray-700">
 
-            Please connect to Web3 first!
+            🔌 Please connect to Web3 first!
          
         </div>
         </div>
