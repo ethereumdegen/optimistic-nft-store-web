267309cf67e13236dcfec0bec9de5450a0034442
andy <admazzola@gmail.com>
2021-04-15 15:25:14 -0400

    footer

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -8,9 +8,8 @@
           <h3 class=" text-lg font-bold ">Documentation</h3>
 
     <a href="https://etherscan.io/address/0x167152a46e8616d4a6892a6afd8e52f060151c70" target="_blank" class="no-underline text-sm text-gray-200 ">Guild Contract</a>
-         
-
-
+   <a href="https://etherscan.io/address/0x657223e3fdf539d92c40664db340097d5d6bd9f5" target="_blank" class="no-underline text-sm text-gray-200 ">Reserve Token</a>
+        
         
           </div>
         <div class="w-1/2 lg:w-1/4">
