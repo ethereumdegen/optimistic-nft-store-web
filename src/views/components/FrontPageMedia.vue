2dc1ca143ded31173bf54b54699ab234a941df47
andy <admazzola@gmail.com>
2021-04-15 21:31:17 -0400

    adding sponsor

--- src/views/components/FrontPageMedia.vue
+++ src/views/components/FrontPageMedia.vue
@@ -2,9 +2,20 @@
 <div class="   pt-8 relative pb-2 mb-12 text-white" style="min-height:300px">
             
             <div class="text-lg"> Guild Sponsors </div>    
+ 
 
 
-              <div class="text-sm"> Coming soon... </div>          
+            <div class="my-4 inline-block " >
+                
+                <a class="border-2 border-gray-500 p-2 hover:bg-gray-600" href="https://deploy.art" target="_blank" > 
+                    <img src="@/assets/images/squiggle.png" class="inline-block" width="24px" />
+                    <div class="inline-block ml-2 text-white ">Deploy.Art</div>
+                    
+                    
+              </a> 
+                
+           </div> 
+
 
  </div>
  </template>
